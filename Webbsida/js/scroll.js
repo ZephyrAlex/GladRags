ff7e1abef9e97addb3d44939bc4cee8d74036bca
Mattias Gustafsson <matte_gsson@hotmail.com>
2017-03-02 13:07:53 +0100

    Webbsida - Mattias
    Jag la till filerna till webbsidan

---
+++ Webbsida/js/scroll.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    // Animate the scroll to top
+    $('.go-top').click(function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: 0
+        }, 300);
+    })
+});
